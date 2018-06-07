d169b06ad4021e2698558efee2771005a902ad17
rameshkkumar7 <ramesh.krishna@promonsoft.com>
2018-06-07 17:44:29 +0530

    Changed to php page and front page sections updated

---
+++ index.php
@@ -0,0 +1,382 @@
+<?php include('header.php'); ?>
+    <!-- start banner Area -->
+       <!-- start banner Area -->
+       <section id="home">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/slide2.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/slide1.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/slide3.jpg" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
+    <!-- End banner Area -->
+
+    <!-- Start feature Area -->
+    <section class="feature-area section-gap" id="company_profile">
+        <div class="container">
+            <div class="row">
+                <div class="single-feature col-lg-12">
+                    <h2>Company Profile</h2>
+                    </br>
+                    <p>
+                        <strong style="color: crimson">PROMON</strong> is a leading provider of OSS & BSS Solutions for Telecom Operators around the world. We understand business as much as technology, and help their customers improve their profitability
+                        and efficiency by providing high-value technology consulting, project management outsourcing services. We offer advisory services, managed services and application Monitoring for various telecom solutions including Online charging
+                        systems,Policy Engines(PCRF) and Telecom Billing packages. We have specific expertise in Comverse One(Arbor Kenan), Cerillion Revenue Manager & Openet PCRF and have successfully implemented several solutions on these products.
+                        We also offer expertise in Billing Migration between Cerillion and Comverse One.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End feature Area -->
+
+    <!-- Start testimonial Area -->
+    <section class="testimonial-area relative section-gap">
+        <div class="overlay overlay-bg"></div>
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="menu-content pb-60 col-lg-12">
+                    <div class="title text-center">
+                        <h1 class="mb-10 text-white">WHO WE ARE</h1>
+                        <h3 class="text-white">STRATEGIC VISION, REALISTIC GOALS AND A COMMITTED PURSUIT OF EXCELLENCE IS WHAT PROMONSOFT OFFERS ITS CLIENTS.</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+			<!-- COLUMN 1 - BIG MESSAGE ABOUT THE COMPANY-->
+			    <div class="col-lg-12 col-md-12 column zerif_about_us_center well col-sm-4" data-scrollreveal="enter bottom after 0s over 1s" data-sr-init="true" data-sr-complete="true">
+                    <ul class="text-white" style="text-align:left"> 
+                        <li type="square"> Promon is a company formed by a group of IT &amp; Telecom experts based out of Chennai, India </li> 
+                        <li type="square"> We are a services oriented company offering professional &amp; managed services  for many verticals </li> 
+                        <li type="square"> We have expertise in the entire BSS stack (Billing, Rating, Charging, Policy ,mediation, Provisioning) and have worked on various Commercial off the shelf packages </li> 
+                        <li type="square">We offer Product &amp; tariff configuration expertise in multiple billing systems like Cerillion &amp; Comverse One.</li> 
+                        <li type="square"> We currently manage the Comverse One Online Converged billing platform for a European operator.</li>  
+                        <li type="square"> We have experience in working in US &amp; European markets and bring a professional approach towards our client partners.</li> 
+                        <li type="square"> We have a track record of managing many real time systems </li>
+                    </ul></div>
+
+	</div>
+        </div>
+    </section>
+    <!-- End testimonial Area -->
+
+    <!-- Start offered Area -->
+    <!--<section class="offered-area" id="Managed_Services">
+        <div class="container">
+            <div class="menu-content pb-60 col-lg-8">
+                <div class="title text-center">
+                    <h1 class="mb-10 text-white">Our Offered Services</h1>
+                    <p class="text-white">Who are in extremely love with eco friendly system.</p>
+                </div>
+            </div>
+            <div class="row align-items-center justify-content-start">
+                <div class="col-lg-6 offered-left">
+                    <div class="offer-wrap flex-row d-flex row">
+                        <div class="col-4">
+                            <div class="single-offer mb-30">
+                                <a href="#"></a>
+                                <img class="img-fluid" src="img/o1.png" alt="">
+                                </d>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="single-offer mb-30">
+                                <a href="#"></a>
+                                <img class="img-fluid" src="img/o2.png" alt="">
+                                </d>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="single-offer mb-30">
+                                <a href="#"></a>
+                                <img class="img-fluid" src="img/o3.png" alt="">
+                                </d>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="single-offer">
+                                <a href="#"></a>
+                                <img class="img-fluid" src="img/o4.png" alt="">
+                                </d>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="single-offer">
+                                <a href="#"></a>
+                                <img class="img-fluid" src="img/o5.png" alt="">
+                                </d>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="single-offer">
+                                <a href="#"></a>
+                                <img class="img-fluid" src="img/o6.png" alt="">
+                                </d>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 offered-right">
+                    <h2>Managed Services</h2>
+                    <p class="pt-20 pb-20">
+                        Network Operators are facing unprecedented competition from device manufacturers, innovative internet service providers and rich content and media providers as data ARPU accelerates to exceed voice.
+                    </p>
+                    <a href="#" class="primary-btn header-btn text-uppercase">View Details</a>
+                </div>
+            </div>
+        </div>
+    </section>
+ 
+
+    <section class="facts-area section-gap">
+        <div class="container">
+            <div class="row">
+                <div class="col single-fact">
+                    <h1 class="counter">2536</h1>
+                    <p>Projects Completed</p>
+                </div>
+                <div class="col single-fact">
+                    <h1 class="counter">6784</h1>
+                    <p>Really Happy Clients</p>
+                </div>
+                <div class="col single-fact">
+                    <h1 class="counter">1059</h1>
+                    <p>Total Tasks Completed</p>
+                </div>
+                <div class="col single-fact">
+                    <h1 class="counter">2239</h1>
+                    <p>Cups of Coffee Taken</p>
+                </div>
+                <div class="col single-fact">
+                    <h1 class="counter">435</h1>
+                    <p>In House Professionals</p>
+                </div>
+            </div>
+        </div>
+    </section>-->
+    <!-- end fact Area -->
+
+<section class="focus section-gap" id="focus">
+   <div class="container">
+      <!-- SECTION HEADER -->
+      <div class="section-header">
+         <!-- SECTION TITLE -->
+         <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-60 col-lg-12">
+                <div class="title text-center">
+                    <h1 class="dark-text">Our Core Competencies</h1>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="row">
+         <div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+            <div class="service-icon">
+               <a href="#">
+                  <i class="pixeden" style="background:url(img/comverse.jpg) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+               </a>
+            </div>
+            <h5 class="red-border-bottom">Comverse Billing </h5>
+            <!-- FOCUS HEADING -->
+            <p></p>
+            <ul style="text-align:left">
+               <li> Managed Services</li>
+               <li> 24 x 7 Monitoring </li>
+               <li> PCAT Configuration </li>
+               <li> Migration Assistance </li>
+               <li> Rating &amp; Billing Invoicing expertise </li>
+            </ul>
+            <p></p>
+         </div>
+         <div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+            <div class="service-icon">
+               <a href="#">
+                  <i class="pixeden" style="background:url(img/cerillion.jpg) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+               </a>
+            </div>
+            <h5 class="red-border-bottom">Cerillion Billing</h5>
+            <!-- FOCUS HEADING -->
+            <p></p>
+            <ul style="text-align:left">
+               <li> Managed Services </li>
+               <li> Product Configuration </li>
+               <li> 24 x 7 Monitoring </li>
+               <li> Resolve Data issues </li>
+               <li> Migration </li>
+               <li> Expertise in Cerillion Suite including TNAPS </li>
+               <p></p>
+            </ul>
+         </div>
+         <div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+            <div class="service-icon">
+               <a href="#">
+                  <i class="pixeden" style="background:url(img/openet1.jpg) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+               </a>
+            </div>
+            <h5 class="red-border-bottom">Openet PCRF &amp; Charging</h5>
+            <!-- FOCUS HEADING -->
+            <p></p>
+            <ul style="text-align:left">
+               <li> Use case Analysis &amp; Design </li>
+               <li> Deploy new use cases in PCRF </li>
+               <li> High availability &amp; Load balancing </li>
+               <li> Diameter protocol Expertise (Gx/Gy/ Sy/Rx) </li>
+            </ul>
+            <p></p>
+         </div>
+         <div class="col-lg-3 col-sm-3 focus-box" data-scrollreveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+            <div class="service-icon">
+               <a href="#">
+                  <i class="pixeden" style="background:url(img/EMM.png) no-repeat center;width:100%; height:100%;"></i> <!-- FOCUS ICON-->
+               </a>
+            </div>
+            <h5 class="red-border-bottom">Ericsson Multi Mediation</h5>
+            <!-- FOCUS HEADING -->
+            <p></p>
+            <ul style="text-align:left">
+               <li>  Voice &amp; Data mediation </li>
+               <li> Configure and deploy jobs </li>
+               <li> Expertise in TAP </li>
+               <li> Well-versed with different file formats including binary </li>
+            </ul>
+            <p></p>
+         </div>
+      </div>
+   </div>
+   <!-- / END CONTAINER -->
+</section>
+
+    <!-- Start info Area -->
+    <section class="info-area service-area section-gap relative" id="careers">
+    <div class="overlay overlay-bg"></div>
+        <div class="container-fluid">
+            <div class="row d-flex justify-content-end align-items-center">
+                <div class="col-lg-4 col-md-6 info-left no-padding">
+                    <img class="img-fluid" src="img/careers.png" alt="">
+                </div>
+                <div class="col-lg-6 col-md-6 info-right no-padding">
+                    <h2 class="text-white">Careers</h2>
+                    </br>
+                    <p class="text-white">
+                        The following are the current openings at PROMON.
+                        </br>
+                        <a href='#' class="btn btn-link text-white">Oracle DBA</a>
+                        </br>
+                        <a href='#' class="btn btn-link text-white">Tier 1 Application Support</a>
+                        </br>
+                        Keep checking this page for future job openings.</br> You can always send in your resume to</br> <a href='#' class="btn btn-link text-white">careers@promonsoft.com</a>
+                    </p>
+                    <!-- <button class="primary-btn mt-20 text-uppercase">Learn More<span class="lnr lnr-arrow-right"></button> -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End info Area -->
+
+    <!-- Start about Area -->
+    <!-- <section class="about-area">
+		<div class="container-fluid">
+			<div class="row no-padding justify-content-center d-flex align-items-center">
+				<div class="col-lg-6 about-left">
+					<h1>
+						We Believe that
+						<br> Interior beautifies the
+						<br> Total Architecture
+					</h1>
+					<p class="mt-30">
+						inappropriate behavior is often laughed off as “boys will be boys,”
+						<br> women face higher conduct standards especially in the workplace.
+						<br> That’s why it’s crucial that, as women, our behavior on the job is beyond
+						<br> reproach. inappropriate behavior is often laughed off as “boys will
+						<br> be boys,” women face higher conduct standards especially in the
+						<br> workplace. That’s why it’s crucial that.
+					</p>
+					<button class="primary-btn mt-20 text-uppercase">See Details
+						<span class="lnr lnr-arrow-right">
+					</button>
+				</div>
+				<div class="col-lg-6 about-right no-padding relative">
+					<div class="overlay overlay-bg"></div>
+					<a href="http://www.youtube.com/watch?v=0O2aH4XLbto" class="play-btn">
+						<img class="play" src="img/play-btn.png" alt="">
+					</a>
+					<img class="main img-fluid" src="img/video-bg.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</section> -->
+    <!-- End about Area -->
+
+    <!-- Start brand Area -->
+    <!-- <section class="brand-area">
+		<div class="container">
+			<div class="row logo-wrap">
+				<a class="col single-img" href="#">
+					<img class="d-block mx-auto" src="img/l1.png" alt="">
+				</a>
+				<a class="col single-img" href="#">
+					<img class="d-block mx-auto" src="img/l2.png" alt="">
+				</a>
+				<a class="col single-img" href="#">
+					<img class="d-block mx-auto" src="img/l3.png" alt="">
+				</a>
+				<a class="col single-img" href="#">
+					<img class="d-block mx-auto" src="img/l4.png" alt="">
+				</a>
+				<a class="col single-img" href="#">
+					<img class="d-block mx-auto" src="img/l5.png" alt="">
+				</a>
+			</div>
+		</div>
+	</section> -->
+    <!-- End brand Area -->
+
+    <!-- start contact Area -->
+    <section class="contact-area section-gap" id="contact">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="menu-content pb-30 col-lg-8">
+                    <div class="title text-center">
+                        <h1 class="mb-10">If you need, Just drop us a line</h1>
+                        <p>Who are in extremely love with eco friendly system.</p>
+                    </div>
+                </div>
+            </div>
+            <form class="form-area mt-60" id="myForm" action="mail.php" method="post" class="contact-form text-right">
+                <div class="row">
+                    <div class="col-lg-6 form-group">
+                        <input name="name" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" class="common-input mb-20 form-control" required="" type="text">
+
+                        <input name="email" placeholder="Enter email address" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" class="common-input mb-20 form-control" required="" type="email">
+
+                        <input name="subject" placeholder="Enter your subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your subject'" class="common-input mb-20 form-control" required="" type="text">
+                    </div>
+                    <div class="col-lg-6 form-group">
+                        <textarea class="common-textarea mt-10 form-control" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" required=""></textarea>
+                        <button class="primary-btn mt-20">Send Message
+							<span class="lnr lnr-arrow-right"></span>
+						</button>
+                        <div class="mt-10 alert-msg">
+                        </div>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </section>
+    <!-- end contact Area -->
+<?php include('footer.php'); ?>
